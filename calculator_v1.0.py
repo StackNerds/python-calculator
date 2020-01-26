bab829bceb07fe997c7e3016b417a247017c7d45
fenn-cs <fenn25.fn@gmail.com>
2020-01-26 15:23:19 +0100

    simple python calculator version 1.0
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- calculator_v1.0.py
+++ calculator_v1.0.py
@@ -8,7 +8,6 @@ def substract(first_number, second_number):
 def multiply(first_number, second_number):
     return first_number * second_number
     
-
 def divide(first_number, second_number):
     return first_number/second_number
 
