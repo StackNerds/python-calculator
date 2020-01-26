118135e3c0e87162db98d35b1c6b968c307accfd
fenn-cs <fenn25.fn@gmail.com>
2020-01-26 19:46:34 +0100

    added calculator_v1.1, no * and /
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Here we implement different simple versions of a python calculator, each version
 ### Version 1.0 : Extremely basic
 
 Simply gets two inputs (integers only), calls a function that does the operation on the two inputs using in-built operators to return the result.
+
+### Version 1.1 : Extremely basic, demonstrates multiplication and division without * and /
+
+Same functions as in 1.0, but does not use `*` for multiplication and `/` for division, unlike in 1.0, carrying out a division that has decimal numbers won't work here or return 0.
---
+++ calculator_v1.1.py
@@ -0,0 +1,46 @@
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+def substract(first_number, second_number):
+    return first_number - second_number
+
+def multiply(first_number, second_number):
+    product = 0
+    for i in range(first_number):
+        product = product + second_number
+    return product
+
+def divide(first_number, second_number):
+    quotient = 0
+    if first_number == 0:
+        return 0
+    while first_number >= second_number:
+        first_number = first_number - second_number
+        quotient = quotient + 1
+    return quotient
+
+operations_map = {
+    1: add,
+    2: substract,
+    3: multiply,
+    4: divide
+}
+
+
+print("Hello, enter operation number to carry out operation\n"\
+      "Enter 1 for addition\n" \
+      "Enter 2 for substraction\n" \
+      "Enter 3 for multiplication\n" \
+      "Enter 4 for division\n")
+
+
+operation = int(input("Select operation waiting .... :"))
+first_number = int(input("Enter first number :"))
+second_number = int(input("Enter second number :"))
+
+print(operations_map[operation](first_number, second_number))
+
+
+ 
+
